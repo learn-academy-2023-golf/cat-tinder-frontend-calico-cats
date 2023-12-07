8deb4e1c09443b30e60ea87f517ab6d3b8eff273
Rique <aguirree1985@gmail.com>
2023-12-07 11:31:36 -0800

    Added tests for CatIndex

--- src/__tests__/CatIndex.test.js
+++ src/__tests__/CatIndex.test.js
@@ -3,16 +3,31 @@ import CatIndex from '../pages/CatIndex';
 import { BrowserRouter } from 'react-router-dom';
 import mockCats from '../mockCats';
 
+
 describe("<CatIndex />", () => {
-    it("renders cat cards", () => {
+    beforeEach(() => {
         render(
-            <BrowserRouter>
-                <CatIndex cats = {mockCats}/> 
-            </BrowserRouter>
+        <BrowserRouter>
+            <CatIndex cats={mockCats}/> 
+        </BrowserRouter>    
         )
+    })
+    it("renders cat name", () => {
         mockCats.forEach(cat => {
             const catName = screen.getByText(cat.name)
             expect(catName).toBeInTheDocument()
         })
-    })       
+    })
+    it("renders cat's age'", () =>{
+        mockCats.forEach(cat => {
+            const catAge = screen.getByText(`Age: ${cat.age}`)
+            expect(catAge).toBeInTheDocument()
+        })
+    })
+    it("renders an image'", () =>{
+        mockCats.forEach(cat => {
+            const image = screen.getByAltText(`profile of a cat named ${cat.name}`)
+            expect(image).toBeInTheDocument()
+        })
+    })      
 })
