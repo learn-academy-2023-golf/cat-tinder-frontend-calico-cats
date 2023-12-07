47ea9b3e0037933724bc11cc6e9bfd86a2d41867
Sean <swbriski@gmail.com>
2023-12-07 15:51:09 -0800

    completed create-functionality and some styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import './App.css';
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router';
+// Components
 import Header from './components/Header';
 import Footer from './components/Footer';
+// Pages
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import CatEdit from './pages/CatEdit';
 import CatIndex from './pages/CatIndex';
 import CatNew from './pages/CatNew';
 import CatShow from './pages/CatShow';
-import { Routes, Route } from 'react-router';
-import './App.css';
-
+// Mock Cats
 import mockCats from "./mockCats"
 
 function App() {
+
   const [cats, setCats] = useState(mockCats)
+
+  const createCat = (cat) => {
+    console.log(cat)
+  }
+
   return (
       <>
         <Header/>
@@ -22,7 +29,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/catindex" element={<CatIndex cats={cats} />} />
           <Route path="/catshow/:id" element={<CatShow cats={cats} />} />
-          <Route path="/catnew" element={<CatNew />} />
+          <Route path="/catnew" element={<CatNew createCat={createCat} />} />
           <Route path="/catedit" element={<CatEdit />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
